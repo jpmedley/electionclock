35aa9b2440338ed7935aeafd73b9ae8dc54989df
Joe Medley <jmedley@chromium.org>
2017-01-21 14:45:55 -0800

    Add a service worker.

--- gulpfile.js
+++ gulpfile.js
@@ -25,39 +25,10 @@ gulp.task('msw', ['make-service-worker'], function(callback){
 gulp.task('make-service-worker', function(callback) {
 
 	swPrecache.write(path.join(rootDir, 'serviceworker.js'), {
-		cacheId: 'redder' + version,
+		cacheId: 'electionclock' + version,
 		staticFileGlobs: [rootDir + '/**/*.{html,css,png,jpg,gif}',
 		                  rootDir + '/js/*.js'],
 		stripPrefix: rootDir,
-		navigateFallback: 'message.html',
-		runtimeCaching: [
-		{
-			urlPattern: /https:\/\/www\.reddit\.com\/api\/subreddits_by_topic.json?query=javascript/,
-			handler: 'cacheOnly',
-			options: {
-				cache: {
-					name: 'subreddits' + version
-				}
-			}
-		},
-		{
-			urlPattern: /https:\/\/www\.reddit\.com\/r\/\w{1,255}\.json/,
-			handler: 'networkFirst',
-			options: {
-				cache: {
-					name: 'titles' + version
-				}
-			}
-		},
-		{
-			urlPattern: /https:\/\/www\.reddit\.com\/r\/javascript\/comments\/\w{6}\/[\w]{0,255}\.json/,
-			handler: 'cacheFirst',
-			options: {
-			  	cache: {
-			     	name: 'articles' + version
-			    }
-			  }
-		}],
 		verbose: true
 	}, callback);
 });
